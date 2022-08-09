8b065eb73eed0fc5713b5b6bbbb4d3642577b5ab
asadbek21coder <asadbekegambergenov21coder@gmail.com>
2022-08-09 06:40:15 -0400

    fixing config

--- service-1/config/config.go
+++ service-1/config/config.go
@@ -54,7 +54,7 @@ func Load() Config {
 
 	config := Config{}
 
-	config.ServiceName = cast.ToString(getOrReturnDefaultValue("SERVICE_NAME", "position_service"))
+	config.ServiceName = cast.ToString(getOrReturnDefaultValue("SERVICE_NAME", "service-1"))
 	config.Environment = cast.ToString(getOrReturnDefaultValue("ENVIRONMENT", DebugMode))
 	config.Version = cast.ToString(getOrReturnDefaultValue("VERSION", "1.0"))
 
